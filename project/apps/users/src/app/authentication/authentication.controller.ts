8c3acf8a23536939f2fc863bfa75ee49049c38b2
Dima Samoshkin <de.metro.samuel@gmail.com>
2023-03-31 17:26:39 +0400

    Добавит обработчики маршрута в AuthenticationController

--- project/apps/users/src/app/authentication/authentication.controller.ts
+++ project/apps/users/src/app/authentication/authentication.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service.js';
+import { CreateUserDto } from './dto/create-user.dto.js';
+import { fillObject } from '@project/util/util-core'
+import { UserRdo } from './rdo/user.rdo.js';
+import { LoginUserDto } from './dto/login-user.dto.js';
+import { LoggedUserRdo } from './rdo/logged-user.rdo.js';
 
 @Controller('authentication')
-export class AuthenticationController {}
+export class AuthenticationController {
+  constructor(
+    private readonly authService: AuthenticationService
+  ) {}
+
+  @Post('register')
+  public async create(@Body() dto: CreateUserDto) {
+    const newUser = await this.authService.register(dto);
+    return fillObject(UserRdo, newUser);
+  }
+
+  @Post('login')
+  public async login(@Body() dto: LoginUserDto) {
+    const verifiedUser = await this.authService.verifyUser(dto);
+    return fillObject(LoggedUserRdo, verifiedUser);
+  }
+
+  @Get(':id')
+  public async show(@Param('id') id: string) {
+    const existUser = await this.authService.getUser(id);
+    return fillObject(UserRdo, existUser);
+  }
+}
