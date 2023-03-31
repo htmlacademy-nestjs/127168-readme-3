7a9f1cdf2271fef38ff70d17fc0a511113c88a35
Dima Samoshkin <de.metro.samuel@gmail.com>
2023-03-31 18:10:29 +0400

    Поправит маршрут

--- project/apps/users/src/app/authentication/authentication.controller.ts
+++ project/apps/users/src/app/authentication/authentication.controller.ts
@@ -6,7 +6,7 @@ import { UserRdo } from './rdo/user.rdo';
 import { LoginUserDto } from './dto/login-user.dto';
 import { LoggedUserRdo } from './rdo/logged-user.rdo';
 
-@Controller('authentication')
+@Controller('auth')
 export class AuthenticationController {
   constructor(
     private readonly authService: AuthenticationService
