64e4ca878ba13fa5335de6b9f2714f62ecc82265
Dima Samoshkin <de.metro.samuel@gmail.com>
2023-03-31 16:59:41 +0400

    Добавит методы регистрации и авторизации пользователя

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { BlogUserMemoryRepository } from '../blog-user/blog-user-memory.repository.js';
+import { BlogUserEntity } from '../blog-user/blog-user.entity.js';
+import { AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG } from './authentication.constant.js';
+import { CreateUserDto } from './dto/create-user.dto.js';
+import { LoginUserDto } from './dto/login-user.dto.js';
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     private readonly blogUserRepository: BlogUserMemoryRepository
   ) {}
+
+  public async register(dto: CreateUserDto) {
+    const {email, login, firstname, lastname, password} = dto;
+
+    const blogUser = {
+      email, login, firstname, lastname,
+      avatar: '', passwordHash: ''
+    };
+
+    const existUser = await this.blogUserRepository
+      .findByEmail(email);
+
+    if (existUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser)
+      .setPassword(password)
+
+    return this.blogUserRepository
+      .create(userEntity);
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const {email, password} = dto;
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    const blogUserEntity = new BlogUserEntity(existUser);
+    if (!await blogUserEntity.comparePassword(password)) {
+      throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return blogUserEntity.toObject();
+  }
+
+  public async getUser(id: string) {
+    return this.blogUserRepository.findById(id);
+  }
 }
